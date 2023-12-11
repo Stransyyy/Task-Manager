29091506610754ae7a30344320d7dcfc67ab9b95
Alan Cano <alancorreonuevo5@gmail.com>
2023-12-11 15:28:25 -0600

    fixed .env and gitignored it

--- application/main.go
+++ application/main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 
+	db "github.com/Stransyyy/Task-Manager/mysql"
 	task "github.com/Stransyyy/Task-Manager/tsk-mngr"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	godotenv.Load("run.env")
+
+	db, err := db.Open()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	fmt.Println("")
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	task.Task_option()
 }
--- application/mysql/connection.go
+++ application/mysql/connection.go
@@ -1,4 +1,4 @@
-package dynamo
+package mysql
 
 import (
 	"database/sql"
@@ -8,14 +8,14 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func Connection() (*sql.DB, error) {
+func Open() (*sql.DB, error) {
 	connectionString := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s", os.Getenv("Username"), os.Getenv("Password"), os.Getenv("Database"))
 
 	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println("Successfully connected")
+	fmt.Println("Successfully connected to the db")
 
 	return db, err
 }
