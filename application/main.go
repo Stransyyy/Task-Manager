189ba6a7f7b498a4548f0976f007a231de1c2a17
Alan Cano <alancorreonuevo5@gmail.com>
2023-12-14 13:51:55 -0600

    Encapsulation

--- application/main.go
+++ application/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"database/sql"
 	"errors"
-	"fmt"
 
 	db "github.com/Stransyyy/Task-Manager/mysql"
 	task "github.com/Stransyyy/Task-Manager/tsk-mngr"
@@ -13,9 +11,11 @@ import (
 type sliceStorage struct {
 	Tasks []*task.Task
 }
-type dbStorage struct {
-	DB *sql.Tx
-}
+
+// type dynamoStorage struct {
+// 	db *dynamo.DB
+// 	Table dynamo.Table
+// }
 
 func (s sliceStorage) GetAll() ([]*task.Task, error) {
 	return s.Tasks, nil
@@ -60,73 +60,23 @@ func (s sliceStorage) Delete(id int) error {
 	return nil
 }
 
-func (db dbStorage) GetAll() ([]*task.Task, error) {
-	rows, err := db.DB.Query("SELECT id, title, description, due_date, completed FROM tasks")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var tasks []*task.Task
-
-	for rows.Next() {
-		var t task.Task
-		err := rows.Scan(&t.ID, &t.Title, &t.Description, &t.DueDate, &t.Completed)
-		if err != nil {
-			return nil, err
-		}
-		tasks = append(tasks, &t)
-	}
-
-	return tasks, nil
-}
-
-func (db dbStorage) Store(t *task.Task) error {
-	_, err := db.DB.Exec("INSERT INTO tasks (title, description, due_date, completed) VALUES (?, ?, ?, ?)", t.Title, t.Description, t.DueDate, t.Completed)
-	return err
-}
-
-func (db dbStorage) MarkCompleted(id int) error {
-	_, err := db.DB.Exec("UPDATE tasks SET completed = true WHERE id = ?", id)
-	return err
-}
-
-func (db dbStorage) Delete(id int) error {
-	_, err := db.DB.Exec("DELETE FROM tasks WHERE id = ?", id)
-	return err
-}
-
-func (db dbStorage) Get(id int) (*task.Task, error) {
-	var t task.Task
-	err := db.DB.QueryRow("SELECT id, title, description, due_date, completed FROM tasks WHERE id = ?", id).Scan(&t.ID, &t.Title, &t.Description, &t.DueDate, &t.Completed)
-	if err != nil {
-	}
-	return &t, nil
-}
-
 func main() {
 
 	godotenv.Load("run.env")
 
-	db, err := db.Open()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer db.Close()
+	//myStorage := sliceStorage{}
 
-	fmt.Println("")
+	dbStorage := db.New()
 
-	err = db.Ping()
+	err := dbStorage.Open()
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
 
-	myStorage := sliceStorage{}
+	defer dbStorage.Close()
 
 	taskManager := task.Tasks{
-		Storage: myStorage,
+		Storage: dbStorage,
 	}
 
 	taskManager.Run()
--- application/mysql/connection.go
+++ application/mysql/connection.go
@@ -2,20 +2,81 @@ package mysql
 
 import (
 	"database/sql"
-	"fmt"
-	"os"
 
+	task "github.com/Stransyyy/Task-Manager/tsk-mngr"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func Open() (*sql.DB, error) {
-	connectionString := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s", os.Getenv("Username"), os.Getenv("Password"), os.Getenv("Database"))
+type Storage struct {
+	db      *sql.DB
+	retries int
+}
 
-	db, err := sql.Open("mysql", connectionString)
+func (db Storage) GetAll() ([]*task.Task, error) {
+	rows, err := db.db.Query("SELECT id, title, description, due_date, completed FROM tasks")
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println("Successfully connected to the db")
+	defer rows.Close()
 
-	return db, err
+	var tasks []*task.Task
+
+	for rows.Next() {
+		var t task.Task
+		err := rows.Scan(&t.ID, &t.Title, &t.Description, &t.DueDate, &t.Completed)
+		if err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, &t)
+	}
+
+	return tasks, nil
+}
+
+func (db Storage) Store(t *task.Task) error {
+	_, err := db.db.Exec("INSERT INTO tasks (title, description, due_date, completed) VALUES (?, ?, ?, ?)", t.Title, t.Description, t.DueDate, t.Completed)
+	return err
+}
+
+func (db Storage) MarkCompleted(id int) error {
+	_, err := db.db.Exec("UPDATE tasks SET completed = true WHERE id = ?", id)
+	return err
+}
+
+func (db Storage) Delete(id int) error {
+	_, err := db.db.Exec("DELETE FROM tasks WHERE id = ?", id)
+	return err
+}
+
+func (db Storage) Get(id int) (*task.Task, error) {
+	var t task.Task
+	err := db.db.QueryRow("SELECT id, title, description, due_date, completed FROM tasks WHERE id = ?", id).Scan(&t.ID, &t.Title, &t.Description, &t.DueDate, &t.Completed)
+	if err != nil {
+		return nil, err
+	}
+	return &t, nil
+}
+
+func (d *Storage) Open() error {
+	db, err := sql.Open("mysql", "root:password@tcp(")
+	if err != nil {
+		return err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+
+	d.db = db
+
+	return nil
+}
+
+func (d Storage) Close() error {
+	return d.db.Close()
+}
+
+func New() *Storage {
+	return &Storage{}
 }
